f3561aa58ad8bee058ef8783fb63636838e23696
CamilaMarin <camila.marin@usach.cl>
2015-05-27 01:38:41 -0400

    Cliente.go y algoritmos

---
+++ Cliente.go
@@ -0,0 +1,168 @@
+// http://golang.org/pkg/net/http/
+package main
+import (
+"net/http"
+"fmt"
+"encoding/json"
+"io/ioutil"
+"log"
+"strconv"
+"sort"
+"math/rand"
+)
+
+type Entrada struct { 
+    Ordenamiento string `json:"ordenamiento"` 
+    Datos []struct { 
+        ID string `json:"id"` 
+    } `json:"datos"` 
+} 
+
+func main() {
+    http.HandleFunc("/solve", handler)
+    err := http.ListenAndServe(":4568", nil)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(err)
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("Content-Type", "text/plain")
+    w.WriteHeader(http.StatusOK)
+    if req.Method == "POST"{
+        body, err := ioutil.ReadAll(req.Body)
+         if nil != err {
+             fmt.Println("errorination happened reading the body", err)
+             return
+         }
+        entrada := Entrada{}
+        json.Unmarshal([]byte(body), &struct {
+            *Entrada
+        }{&entrada})
+        var largo = len(entrada.Datos)
+        result := make([]string, largo)
+        for i := range entrada.Datos {
+            result[i] = entrada.Datos[i].ID
+        }
+        datos := make([]float64, largo)
+        for j := range result{
+            i, err := strconv.ParseFloat(result[j], 64)
+                if err != nil {
+                    // handle error
+                    fmt.Println(err)
+                }
+            datos[j]=i
+        }
+        fmt.Println(datos)   
+        var largo_al = len(entrada.Ordenamiento)
+        result_al := make([]string, largo_al)
+        for i := range entrada.Ordenamiento {
+            result_al[i] = entrada.Ordenamiento
+        }
+        algoritmo, err := strconv.Atoi(result_al[0])
+        if err != nil {
+            // handle error
+            fmt.Println(err)
+        }
+        if algoritmo==1 {
+            quicksort(sort.Float64Slice(datos))
+            fmt.Println(datos)
+        }
+        if algoritmo==2 {
+            mergeSort(datos)
+            fmt.Println(datos)
+        }
+        if algoritmo==3 {
+            bubblesort(datos)
+            fmt.Println(datos)
+        }
+        b, err := json.Marshal(datos)
+        if err != nil {
+            fmt.Println("error:", err)
+        }
+        fmt.Fprintln(w, string(b[:]))
+
+    }
+}
+
+func partition(a sort.Interface, first int, last int, pivotIndex int) int {
+    a.Swap(first, pivotIndex) // move it to beginning
+    left := first+1
+    right := last
+    for left <= right {
+        for left <= last && a.Less(left, first) {
+            left++
+        }
+        for right >= first && a.Less(first, right) {
+            right--
+        }
+        if left <= right {
+            a.Swap(left, right)
+            left++
+            right--
+        }
+    }
+    a.Swap(first, right) // swap into right place
+    return right    
+}
+ 
+func quicksortHelper(a sort.Interface, first int, last int) {
+    if first >= last {
+        return
+    }
+    pivotIndex := partition(a, first, last, rand.Intn(last - first + 1) + first)
+    quicksortHelper(a, first, pivotIndex-1)
+    quicksortHelper(a, pivotIndex+1, last)
+}
+ 
+func quicksort(a sort.Interface) {
+    quicksortHelper(a, 0, a.Len()-1)
+}
+func mergeSort(a []float64) {
+    if len(a) < 2.0 {
+        return
+    }
+    mid := len(a) / 2.0
+    mergeSort(a[:mid])
+    mergeSort(a[mid:])
+    if a[mid-1] <= a[mid] {
+        return
+    }
+    // merge step, with the copy-half optimization
+    s := make([]float64, mid)
+    copy(s, a[:mid])
+    l, r := 0, mid
+    for i := 0; ; i++ {
+        if s[l] <= a[r] {
+            a[i] = s[l]
+            l++
+            if l == mid {
+                break
+            }
+        } else {
+            a[i] = a[r]
+            r++
+            if r == len(a) {
+                copy(a[i+1:], s[l:mid])
+                break
+            }
+        }
+    }
+    return
+}
+
+func bubblesort(a []float64){
+    for itemCount := len(a) - 1.0; ; itemCount-- {
+        hasChanged := false
+        for index := 0; index < itemCount; index++ {
+            if a[index] > a[index+1.0] {
+                a[index], a[index+1.0] = a[index+1.0], a[index]
+                hasChanged = true
+            }
+        }
+        if hasChanged == false {
+            break
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,5 @@ http://code.tutsplus.com/tutorials/singing-with-sinatra--net-18965
 http://stackoverflow.com/questions/17049569/how-to-parse-json-request-body-in-sinatra-just-once-and-expose-it-to-all-routes
 https://gist.github.com/aspyct/3433278
 http://stackoverflow.com/questions/11091434/using-the-bubble-sort-method-for-an-array-in-ruby
+http://rosettacode.org/wiki/Sorting_algorithms
+http://golang.org/pkg/net/http/
